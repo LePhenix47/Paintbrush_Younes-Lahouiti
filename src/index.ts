8b4952ea3d86813dd6e2ab8da3e6af7b5d1ab195
Younes Lahouiti <loller763@gmail.com>
2023-05-21 16:03:27 +0200

    Finalization of all the HTML + CSS of the project, will later start adding the JS code

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import { log } from "./utils/functions/console.functions";
+import { selectQuery } from "./utils/functions/dom.functions";
 
 log("Hello world!");
+
+const canvasPaint: HTMLCanvasElement = selectQuery("canvas.index__canvas");
