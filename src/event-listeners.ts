799cad0bed116d6f9d8188ba1f7be7d9f565015c
Younes Lahouiti <loller763@gmail.com>
2023-05-23 15:15:53 +0200

    Addition of a file to handle the event listeners + JSDoc for each funciton

---
+++ src/event-listeners.ts
@@ -0,0 +1,389 @@
+import { tracker } from "./index";
+import { Interval } from "./utils/classes/services/interval.class";
+import { transformColorModel } from "./utils/functions/color-conversion.functions";
+import { log } from "./utils/functions/console.functions";
+import {
+  getAncestor,
+  selectQuery,
+  getParent,
+  getAttribute,
+} from "./utils/functions/dom.functions";
+import {
+  formatText,
+  kebabToCamelCase,
+} from "./utils/functions/string.functions";
+
+type checkboxHueRotationType = {
+  fill: {
+    animationId: number;
+    hue: number;
+    saturation: number;
+    lightness: number;
+    type: string;
+  };
+  stroke: {
+    animationId: number;
+    hue: number;
+    saturation: number;
+    lightness: number;
+    type: string;
+  };
+};
+const checkboxHueRotation: checkboxHueRotationType = {
+  fill: {
+    animationId: 0,
+    hue: 0,
+    saturation: 0,
+    lightness: 0,
+    type: "fill",
+  },
+  stroke: {
+    animationId: 0,
+    hue: 0,
+    saturation: 0,
+    lightness: 0,
+    type: "stroke",
+  },
+};
+
+/**
+ * Sets the hue rotation to auto based on the checkbox state.
+ *
+ * @param {Event} event - The event triggered by the checkbox change.
+ *
+ * @returns {void}
+ */
+export function setHueRotationAuto(event: Event): void {
+  //@ts-ignore
+  const checkboxInput: HTMLInputElement = event.currentTarget;
+
+  const container: HTMLElement = getAncestor(
+    checkboxInput,
+    ".colors__container"
+  );
+
+  const colorsInput: HTMLInputElement = selectQuery(
+    ".colors__input",
+    container
+  );
+
+  const colorInputValue: string = colorsInput.value;
+
+  const inputId: string = colorsInput.id;
+
+  log(container);
+
+  const { hue, saturation, lightness } = transformColorModel(
+    colorInputValue,
+    "hex",
+    "hsl"
+  );
+
+  //We cehck if the value must be transparent
+  const mustBeTransparent: boolean = checkIfNeedsToBeTransparent(container);
+
+  //If the color is desaturated or if it's a value between white and black
+  const colorIsDesaturated: boolean =
+    saturation === 0 || lightness === 0 || lightness === 100;
+
+  const cannotRotateHue: boolean = mustBeTransparent || colorIsDesaturated;
+
+  if (cannotRotateHue) {
+    return;
+  }
+
+  checkboxHueRotation[inputId].type = inputId;
+
+  checkboxHueRotation[inputId].hue = hue;
+  checkboxHueRotation[inputId].saturation = saturation;
+  checkboxHueRotation[inputId].lightness = lightness;
+
+  const isChecked: boolean = checkboxInput.checked;
+  if (isChecked) {
+    checkboxHueRotation[inputId].animationId = Interval.set(
+      50,
+      convertAndRotateHue,
+      colorsInput,
+      checkboxHueRotation[inputId]
+    );
+  } else {
+    Interval.clear(checkboxHueRotation[inputId].animationId);
+  }
+
+  log(colorsInput);
+}
+
+/**
+ * Converts and rotates the hue value.
+ *
+ * @param {HTMLInputElement} input - The input element.
+ * @param {Object} checkboxObject - The checkbox object.
+ *
+ * @returns {void}
+ */
+export function convertAndRotateHue(
+  input: HTMLInputElement,
+  checkboxObject: {
+    id: number;
+    hue: number;
+    saturation: number;
+    lightness: number;
+    type: string;
+  }
+): void {
+  checkboxObject.hue += 2;
+  checkboxObject.hue %= 360;
+
+  let newHexValue: string = transformColorModel(checkboxObject, "hsl", "hex");
+  newHexValue = formatText(newHexValue, "uppercase");
+
+  const label: HTMLLabelElement = getParent(input);
+  //@ts-ignore
+  input.value = newHexValue;
+  setSpanToInputValue(label, newHexValue);
+
+  tracker[checkboxObject.type] = newHexValue;
+  log(tracker);
+}
+
+/**
+ * Sets the tool to the tracker.
+ *
+ * @param {Event} event - The event triggered by the radio input change.
+ *
+ * @returns {void}
+ */
+export function setToolToTracker(event: Event): void {
+  //@ts-ignore
+  const radioInput: HTMLInputElement = event.currentTarget;
+
+  tracker.tool = radioInput.id;
+  log(tracker);
+}
+
+/**
+ * Sets the number input values to the tracker.
+ *
+ * @param {Event} event - The event triggered by the number input change.
+ *
+ * @returns {void}
+ */
+export function setNumberInputValues(event: Event): void {
+  //@ts-ignore
+  const input: HTMLInputElement = event.currentTarget;
+
+  const inputValueIsInvalid: boolean = isNaN(input.valueAsNumber);
+  if (inputValueIsInvalid) {
+    return;
+  }
+
+  handleInputValueOverflow(input, 0, Number.POSITIVE_INFINITY);
+
+  const label: HTMLLabelElement = getParent(input);
+  const labelForAttribute: string = getAttribute("for", label);
+
+  const formattedAttribute: string = kebabToCamelCase(labelForAttribute);
+
+  tracker[formattedAttribute] = input.valueAsNumber;
+  log(tracker);
+}
+
+/**
+ * Handles input value overflow.
+ *
+ * @param {HTMLInputElement} input - The input element.
+ * @param {number} [min=Number.NEGATIVE_INFINITY] - The minimum value.
+ * @param {number} [max=Number.POSITIVE_INFINITY] - The maximum value.
+ *
+ * @returns {void}
+ */
+export function handleInputValueOverflow(
+  input: HTMLInputElement,
+  min: number = Number.NEGATIVE_INFINITY,
+  max: number = Number.POSITIVE_INFINITY
+): void {
+  const inputValue: number = input.valueAsNumber;
+
+  const valueOverflows: boolean = inputValue > max;
+  if (valueOverflows) {
+    input.valueAsNumber = max;
+  }
+
+  const valueUnderflows: boolean = inputValue < min;
+  if (valueUnderflows) {
+    input.valueAsNumber = min;
+  }
+}
+
+const rangeInputsInfosTracker = {
+  size: {
+    id: 0,
+    direction: "forwards",
+  },
+  rotation: {
+    id: 0,
+    direction: "forwards",
+  },
+};
+
+/**
+ * Updates the range input values based on the checkbox state.
+ *
+ * @param {Event} event - The event triggered by the checkbox change.
+ *
+ * @returns {void}
+ */
+export function updateRangeInputValues(event: Event): void {
+  //@ts-ignore
+  const checkboxInput: HTMLInputElement = event.currentTarget;
+
+  const inputName: string = checkboxInput.name;
+
+  const isChecked: boolean = checkboxInput.checked;
+  if (isChecked) {
+    //@ts-ignore
+    rangeInputsInfosTracker[inputName].id = Interval.set(
+      10,
+      animateInputRange,
+      checkboxInput
+    );
+  } else {
+    //@ts-ignore
+    Interval.clear(rangeInputsInfosTracker[inputName].id);
+  }
+}
+
+/**
+ * Animates the input range value.
+ *
+ * @param {HTMLInputElement} checkboxInput - The checkbox input element.
+ *
+ * @returns {void}
+ */
+export function animateInputRange(checkboxInput: HTMLInputElement): void {
+  const inputName: string = checkboxInput.name;
+
+  const container: HTMLElement = getAncestor(
+    checkboxInput,
+    ".controls__brush-container"
+  );
+  log(container);
+
+  const rangeInput: HTMLInputElement = selectQuery(
+    ".controls__input--range",
+    container
+  );
+
+  const currentValue: number = rangeInput.valueAsNumber;
+  const maxValue: number = Number(rangeInput.max);
+  const minValue: number = Number(rangeInput.min);
+
+  let counter: number = 1;
+
+  const overflows: boolean = currentValue >= maxValue;
+  if (overflows) {
+    rangeInputsInfosTracker[inputName].direction = "backwards";
+  }
+
+  const underflows: boolean = currentValue <= minValue;
+  if (underflows) {
+    rangeInputsInfosTracker[inputName].direction = "forwards";
+  }
+
+  const directionIsForwards: boolean =
+    rangeInputsInfosTracker[inputName].direction === "forwards";
+  if (directionIsForwards) {
+    //@ts-ignore
+    rangeInput.value = currentValue + counter;
+  } else {
+    //@ts-ignore
+    rangeInput.value = currentValue - counter;
+  }
+  const valueWithUnit: string = `${rangeInput.valueAsNumber}`;
+
+  const label = selectQuery("label", container);
+  log(label);
+
+  setSpanToInputValue(container, valueWithUnit);
+}
+
+/**
+ * Sets the range input values.
+ *
+ * @param {Event} event - The event triggered by the input change.
+ *
+ * @returns {void}
+ */
+export function setRangeInputValues(event: Event): void {
+  //@ts-ignore
+  const rangeInput: HTMLInputElement = event.currentTarget;
+  const inputValue: number = rangeInput.valueAsNumber;
+
+  const label: HTMLLabelElement = getParent(rangeInput);
+
+  setSpanToInputValue(label, inputValue);
+}
+
+/**
+ * Sets the color input values.
+ *
+ * @param {Event} event - The event triggered by the input change.
+ *
+ * @returns {void}
+ */
+export function setColorInputValues(event: Event): void {
+  //@ts-ignore
+  const input: HTMLInputElement = event.currentTarget;
+
+  const formattedInputValue: string = formatText(input.value, "uppercase");
+
+  const label: HTMLLabelElement = getParent(input);
+  setSpanToInputValue(label, formattedInputValue);
+
+  const container: HTMLElement = getParent(label);
+
+  const colorCheckboxesDiv = selectQuery("div", container);
+  log(colorCheckboxesDiv);
+
+  const colorType: string = input.id;
+
+  //Check here if color needs to be transparent
+  const needsToBeTransparent: boolean =
+    checkIfNeedsToBeTransparent(colorCheckboxesDiv);
+  if (needsToBeTransparent) {
+    tracker[colorType] = "transparent";
+  } else {
+    tracker[colorType] = formattedInputValue;
+  }
+
+  log(tracker);
+}
+
+/**
+ * Checks if the color needs to be transparent based on the checkbox state.
+ *
+ * @param {HTMLElement} container - The container element.
+ *
+ * @returns {boolean} - Whether the color needs to be transparent or not.
+ */
+export function checkIfNeedsToBeTransparent(container: HTMLElement): boolean {
+  const checkbox: HTMLInputElement = selectQuery(
+    "input[type=checkbox]",
+    container
+  );
+  return !checkbox.checked;
+}
+
+/**
+ * Sets the span element to display the input value.
+ *
+ * @param {HTMLElement} container - The container element.
+ * @param {any} value - The value to be displayed.
+ *
+ * @returns {void}
+ */
+export function setSpanToInputValue(container: HTMLElement, value: any): void {
+  const spanLabel: HTMLSpanElement = selectQuery("span", container);
+
+  spanLabel.textContent = value;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,38 @@
-import { Interval } from "./utils/classes/services/interval.class";
 import {
-  hexColorToRgb,
-  hslColorToHex,
-  hslColorToHwb,
-  rgbColorToHsl,
-  transformColorModel,
-} from "./utils/functions/color-conversion.functions";
-import { assert, count, log, table } from "./utils/functions/console.functions";
+  setColorInputValues,
+  setHueRotationAuto,
+  setNumberInputValues,
+  setRangeInputValues,
+  setToolToTracker,
+  updateRangeInputValues,
+} from "./event-listeners";
+import { assert, log, table } from "./utils/functions/console.functions";
 import {
-  getAncestor,
-  getAttribute,
-  getParent,
-  modifyAttribute,
   selectByClass,
   selectQuery,
   selectQueryAll,
 } from "./utils/functions/dom.functions";
-import {
-  formatText,
-  kebabToCamelCase,
-} from "./utils/functions/string.functions";
 
-const tracker = {
+export const tracker = {
   tool: "brush",
   fill: "#000000",
   stroke: "transparent",
   strokeWidth: 0,
   size: 5,
+  angle: 0,
   isDrawing: false,
   shadow: "#000000",
   shadowBlur: 0,
   shadowOffsetX: 0,
   shadowOffsetY: 0,
   hasShape: false,
-  shape: "none",
+  shape: "line",
   sides: 3,
   innerRadius: 1,
   globalCompositeOperation: "source-over",
 };
 
-const mouseInfos = {
+export const mouseInfos = {
   x: null,
   y: null,
 };
@@ -118,313 +111,3 @@ setControlsContainerEvents();
 function setShapesContainerEvents() {}
 
 function setMiscellaneousContainerEvents() {}
-
-const checkboxHueRotation = {
-  fill: {
-    animationId: 0,
-    hue: 0,
-    saturation: 0,
-    lightness: 0,
-    type: "fill",
-  },
-  stroke: {
-    animationId: 0,
-    hue: 0,
-    saturation: 0,
-    lightness: 0,
-    type: "stroke",
-  },
-};
-
-function setHueRotationAuto(event: Event) {
-  //@ts-ignore
-  const checkboxInput: HTMLInputElement = event.currentTarget;
-
-  const container: HTMLElement = getAncestor(
-    checkboxInput,
-    ".colors__container"
-  );
-
-  const colorsInput: HTMLInputElement = selectQuery(
-    ".colors__input",
-    container
-  );
-
-  const colorInputValue: string = colorsInput.value;
-
-  const inputId: string = colorsInput.id;
-
-  log(container);
-
-  const { hue, saturation, lightness } = transformColorModel(
-    colorInputValue,
-    "hex",
-    "hsl"
-  );
-
-  //We cehck if the value must be transparent
-  const mustBeTransparent: boolean = checkIfNeedsToBeTransparent(container);
-
-  //If the color is desaturated or if it's a value between white and black
-  const colorIsDesaturated: boolean =
-    saturation === 0 || lightness === 0 || lightness === 100;
-
-  const cannotRotateHue: boolean = mustBeTransparent || colorIsDesaturated;
-
-  if (cannotRotateHue) {
-    return;
-  }
-
-  checkboxHueRotation[inputId].type = inputId;
-
-  checkboxHueRotation[inputId].hue = hue;
-  checkboxHueRotation[inputId].saturation = saturation;
-  checkboxHueRotation[inputId].lightness = lightness;
-
-  const isChecked: boolean = checkboxInput.checked;
-  if (isChecked) {
-    checkboxHueRotation[inputId].animationId = Interval.set(
-      50,
-      convertAndRotateHue,
-      colorsInput,
-      checkboxHueRotation[inputId]
-    );
-  } else {
-    Interval.clear(checkboxHueRotation[inputId].animationId);
-  }
-
-  log(colorsInput);
-}
-
-function convertAndRotateHue(
-  input: HTMLInputElement,
-  checkboxObject: {
-    id: number;
-    hue: number;
-    saturation: number;
-    lightness: number;
-    type: string;
-  }
-) {
-  checkboxObject.hue += 2;
-  checkboxObject.hue %= 360;
-
-  let newHexValue: string = transformColorModel(checkboxObject, "hsl", "hex");
-  newHexValue = formatText(newHexValue, "uppercase");
-
-  const label: HTMLLabelElement = getParent(input);
-  //@ts-ignore
-  input.value = newHexValue;
-  setSpanToInputValue(label, newHexValue);
-
-  tracker[checkboxObject.type] = newHexValue;
-  log(tracker);
-}
-
-function setToolToTracker(event: Event) {
-  //@ts-ignore
-  const radioInput: HTMLInputElement = event.currentTarget;
-
-  tracker.tool = radioInput.id;
-  log(tracker);
-}
-
-function setNumberInputValues(event: Event) {
-  //@ts-ignore
-  const input: HTMLInputElement = event.currentTarget;
-
-  const inputValueIsInvalid: boolean = isNaN(input.valueAsNumber);
-  if (inputValueIsInvalid) {
-    return;
-  }
-
-  handleInputValueOverflow(input, 0, Number.POSITIVE_INFINITY);
-
-  const label: HTMLLabelElement = getParent(input);
-  const labelForAttribute: string = getAttribute("for", label);
-
-  const formattedAttribute: string = kebabToCamelCase(labelForAttribute);
-
-  tracker[formattedAttribute] = input.valueAsNumber;
-  log(tracker);
-}
-
-function handleInputValueOverflow(
-  input: HTMLInputElement,
-  min: number = Number.NEGATIVE_INFINITY,
-  max: number = Number.POSITIVE_INFINITY
-) {
-  const inputValue: number = input.valueAsNumber;
-
-  const valueOverflows: boolean = inputValue > max;
-  if (valueOverflows) {
-    input.valueAsNumber = max;
-  }
-
-  const valueUnderflows: boolean = inputValue < min;
-  if (valueUnderflows) {
-    input.valueAsNumber = min;
-  }
-}
-
-const rangeInputsInfosTracker = {
-  size: {
-    id: 0,
-    direction: "forwards",
-  },
-  rotation: {
-    id: 0,
-    direction: "forwards",
-  },
-};
-
-/**
- * Updates the range input values based on the checkbox state.
- *
- * @param {Event} event - The event triggered by the checkbox change.
- *
- * @returns {void}
- */
-function updateRangeInputValues(event: Event): void {
-  //@ts-ignore
-  const checkboxInput: HTMLInputElement = event.currentTarget;
-
-  const inputName: string = checkboxInput.name;
-
-  const isChecked: boolean = checkboxInput.checked;
-  if (isChecked) {
-    //@ts-ignore
-    rangeInputsInfosTracker[inputName].id = Interval.set(
-      10,
-      animateInputRange,
-      checkboxInput
-    );
-  } else {
-    //@ts-ignore
-    Interval.clear(rangeInputsInfosTracker[inputName].id);
-  }
-}
-
-/**
- * Animates the input range value.
- *
- * @param {HTMLInputElement} checkboxInput - The checkbox input element.
- *
- * @returns {void}
- */
-function animateInputRange(checkboxInput: HTMLInputElement): void {
-  const inputName: string = checkboxInput.name;
-
-  const container: HTMLElement = getAncestor(
-    checkboxInput,
-    ".controls__brush-container"
-  );
-
-  const rangeInput: HTMLInputElement = selectQuery(
-    ".controls__input--range",
-    container
-  );
-
-  const currentValue: number = rangeInput.valueAsNumber;
-  const maxValue: number = Number(rangeInput.max);
-  const minValue: number = Number(rangeInput.min);
-
-  let counter: number = 1;
-
-  const overflows: boolean = currentValue >= maxValue;
-  if (overflows) {
-    rangeInputsInfosTracker[inputName].direction = "backwards";
-  }
-
-  const underflows: boolean = currentValue <= minValue;
-  if (underflows) {
-    rangeInputsInfosTracker[inputName].direction = "forwards";
-  }
-
-  const directionIsForwards: boolean =
-    rangeInputsInfosTracker[inputName].direction === "forwards";
-  if (directionIsForwards) {
-    //@ts-ignore
-    rangeInput.value = currentValue + counter;
-  } else {
-    //@ts-ignore
-    rangeInput.value = currentValue - counter;
-  }
-  const valueWithUnit: string = `${rangeInput.value}`;
-
-  setSpanToInputValue(container, valueWithUnit);
-}
-
-/**
- * Sets the range input values.
- *
- * @param {Event} event - The event triggered by the input change.
- *
- * @returns {void}
- */
-function setRangeInputValues(event: Event): void {
-  //@ts-ignore
-  const rangeInput: HTMLInputElement = event.currentTarget;
-  const inputValue: number = rangeInput.valueAsNumber;
-
-  const label: HTMLLabelElement = getParent(rangeInput);
-
-  setSpanToInputValue(label, inputValue);
-}
-
-/**
- * Sets the color input values.
- *
- * @param {Event} event - The event triggered by the input change.
- *
- * @returns {void}
- */
-function setColorInputValues(event: Event): void {
-  //@ts-ignore
-  const input: HTMLInputElement = event.currentTarget;
-
-  const formattedInputValue: string = formatText(input.value, "uppercase");
-
-  const label: HTMLLabelElement = getParent(input);
-  setSpanToInputValue(label, formattedInputValue);
-
-  const container: HTMLElement = getParent(label);
-
-  const colorCheckboxesDiv = selectQuery("div", container);
-  log(colorCheckboxesDiv);
-
-  const colorType: string = input.id;
-
-  //Check here if color needs to be transparent
-  const needsToBeTransparent: boolean =
-    checkIfNeedsToBeTransparent(colorCheckboxesDiv);
-  if (needsToBeTransparent) {
-    tracker[colorType] = "transparent";
-  } else {
-    tracker[colorType] = formattedInputValue;
-  }
-
-  log(tracker);
-}
-
-function checkIfNeedsToBeTransparent(container: HTMLElement): boolean {
-  const checkbox: HTMLInputElement = selectQuery(
-    "input[type=checkbox]",
-    container
-  );
-  return !checkbox.checked;
-}
-
-/**
- * Sets the span element to display the input value.
- *
- * @param {any} container - The container element.
- * @param {any} value - The value to be displayed.
- *
- * @returns {void}
- */
-function setSpanToInputValue(container: HTMLElement, value: any): void {
-  const spanLabel: HTMLSpanElement = selectQuery("span", container);
-
-  spanLabel.textContent = value;
-}
